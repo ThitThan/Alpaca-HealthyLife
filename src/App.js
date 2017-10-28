7bdc81ae6b258301cc6ede336a0be28e1cd47f3d
f r e s h <fresh.thit@me.com>
2017-10-28 11:56:53 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './components/dist/css/NavTop.css'
+import NavTop from './components/NavTop'
+
+const App = () => (
+  <div>
+    <NavTop/>
+  </div>
+)
+
+export default App
