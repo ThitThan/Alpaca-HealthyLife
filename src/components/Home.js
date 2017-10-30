da88f031b81bf03341b49f612d656d9291704274
Nawapadone <32672811+Nawapadone@users.noreply.github.com>
2017-10-31 00:52:09 +0700

    pictute fitt

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,6 @@
 import React from 'react'
-import './dist/css/bootstrap.css'
 import './shared.css'
-import In01 from './img/in01.jpg' 
-import In02 from './img/in02.jpg' 
-import In03 from './img/in03.jpg' 
-
+import Fitt from './Fitt'
 class Home extends React.Component {
     render() {
         return (
@@ -13,15 +9,9 @@ class Home extends React.Component {
                 <h1>HEALTHY LIFE</h1>
                 {/* <p>u n d e r   c o n s t r u c t i o n</p> */}
                 <br/>
-    <div  className="container">
-        <div  className="container">
-            <div className="row">
-                <div className="col-4"> <img src={In01} className="fitt"/></div>
-                <div className="col-4"><img src={In02} className="fitt"/></div>
-                <div className="col-4"><img src={In03} className="fitt"/></div>
-            </div>
-        </div>
-    </div>
+    
+        <Fitt/>
+   
             </div>
         );
     }
