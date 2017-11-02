395012f73b84305ddd85638e2f69166238b46e8a
Nawapadone <Nawapadone.c@mail.kmutt.ac.th>
2017-11-02 15:48:57 +0700

    navbar

--- src/components/About.js
+++ src/components/About.js
@@ -4,14 +4,19 @@ import aboutweb from './img/aboutweb.png'
 import Aboutphone from './img/aboutphone.png'
 
 
+
 // import Hungry from './healthylife/pages/fact/facthungry'
 
 class About extends React.Component {
     render() {
         return (
             <div>
+                <div>
                 <img src={aboutweb} className="image1 bg"/>
                 <img src={Aboutphone} className="image2 bg"/>
+                
+            </div>
+            
             </div>
         );
     }
