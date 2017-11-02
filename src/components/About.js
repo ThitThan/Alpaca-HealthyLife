605a885d49b506c08782ab90d1f588ff04beb70f
Nawapadone <Nawapadone.c@mail.kmutt.ac.th>
2017-11-02 15:00:59 +0700

    ja

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './shared.css'
 import aboutweb from './img/aboutweb.png'
+import Aboutphone from './img/aboutphone.png'
+
 
 // import Hungry from './healthylife/pages/fact/facthungry'
 
@@ -8,7 +10,8 @@ class About extends React.Component {
     render() {
         return (
             <div>
-                <img src={aboutweb} className="bg"/>
+                <img src={aboutweb} className="image1 bg"/>
+                <img src={Aboutphone} className="image2 bg"/>
             </div>
         );
     }
