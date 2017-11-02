72d5e431de216189dd1cd4503fd6e1c0b9873c97
Unknown <l2jew555@hotmail.com>
2017-11-02 16:07:35 +0700

    asdasdasd

---
+++ src/components/healthykitchenpage.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Footer from '../components/footer'
+
+import './dist/bootstrap-4.0.0-beta.2/dist/css/bootstrap.min.css'
+import Timsom from '../static/img/ซาลาเปาไส้กุ้งสับ.jpg'
+import Kaiob from '../static/img/ไก่อบผลไม้.jpg'
+import rice from "../static/img/ข้าวไรท์เบอรี่มูน+มะม่วงน้ำดอกไม้.jpg" 
+import muffin from "../static/img/bananamuffin.png"
+import bnnt from "../static/img/กล้วยหอมท็อปปิ้ง.jpg"
+import dolly from "../static/img/ดอลลี่อบไส้ผลไม้.jpg"
+
+const healthykitchenpage = () => (
+
+<div>
+<div className = "container">
+<p className = "fontcolorgrey fontsize-45px Akrobat">Healthy Kitchen</p>
+<br className = "color-blue"/>
+    <div className="row">
+        <div className="col-xs-12 col-lg-4" style={{ marginTop: '20px' }}>
+            <img className = "fit2" src={Timsom} width = "320px" height = "280px"/>
+            <br/><br/>
+            <p className = "Nithan text-left fontsize-30px">ซาลาเปากุ้งสับ
+            <Link prefetch to="/kitchen/steambuns">
+            <button type="button" className="btn float-right Nithan color-button">view more</button>
+            </Link>
+            </p>
+            
+        </div>
+        <div className="col-xs-12 col-lg-4" style={{ marginTop: '20px' }}>
+            <img className = "fit2" src={Kaiob} width = "320px" height = "280px"/>
+            <br/><br/>
+            <p className = "Nithan text-left fontsize-30px">ไก่อบผลไม้
+            <Link prefetch to="/kitchen/steakchicken.js">
+            <button type="button" className="btn pull-right Nithan color-button">view more</button>
+            </Link>
+            </p>
+
+        </div>
+        <br/><br/>
+        <div className="col-xs-12 col-lg-4" style={{ marginTop: '20px' }}>
+            <img className = "fit2" src = {rice} width = "320px" height = "280px"/>
+            <br/><br/>
+            <p className = "Nithan text-left fontsize-30px">ข้าวเหนียวมะม่วง
+            <Link prefetch to="/kitchen/riceberry+mango.js">
+            <button type="button" className="btn pull-right Nithan color-button">view more</button>
+            </Link>
+            </p>
+
+        </div>
+    </div>
+    <br/><br/>
+    <div className="row">
+        <div className="col-xs-12 col-lg-4" style={{ marginTop: '20px' }}>
+            <img className = "fit2" src = {muffin}  width = "320px" height = "280px"/>
+            <br/><br/>
+            <p className = "Nithan text-left fontsize-30px">เค้กกล้วยหอม
+            <Link prefetch to="/kitchen/bananamuffin.js">
+            <button type="button" className="btn pull-right Nithan color-button">view more</button>
+            </Link>
+            </p>
+        </div>
+        <div className="col-xs-12 col-lg-4" style={{ marginTop: '20px' }}>
+            <img className = "fit2" src = {bnnt} width = "320px" height = "280px"/>
+            <br/><br/>
+            <p className = "Nithan text-left fontsize-30px">กล้วยหอมท็อปปิ้งธัญพืช
+            <Link prefetch to="/kitchen/bananatopping.js">
+            <button type="button" className="btn pull-right Nithan color-button">view more</button>
+            </Link>
+            </p>
+        </div>
+        <div className="col-xs-12 col-lg-4"style={{ marginTop: '20px' }}>
+            <img className = "fit2" src = {dolly} width = "320px" height = "280px"/>
+            <br/><br/>
+            <p className = "Nithan text-left fontsize-30px">ดอลลี่อบไส้ผลไม้
+            <Link prefetch to="/kitchen/dolly.js">
+            <button type="button" className="btn pull-right Nithan color-button">view more</button>
+            </Link>
+            </p>
+        </div>
+    </div>
+</div>
+<br/>   
+<Footer/>
+</div>
+)
+
+export default healthykitchenpage
